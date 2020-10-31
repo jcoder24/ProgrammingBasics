683473519638d3b860e0a68e098a3dbe34a035d2
ashish2dev <72452676+ashish2dev@users.noreply.github.com>
2020-10-31 15:11:35 +0530

    C# helloworld program without any namespace

---
+++ HelloWorld-WithoutSystemNamespace.cs
@@ -0,0 +1,11 @@
+namespace HelloWorld_WithoutSystemNamespace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World");
+            System.Console.ReadKey();
+        }
+    }
+}
